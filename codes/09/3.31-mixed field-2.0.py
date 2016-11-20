5ce8c04b81c3b1f13696370681c38288b3031ebf
Guibeen <M571575860@outlook.com>
2016-11-21 01:20:57 +0800

    Create 3.31-mixed field-2.0.py

---
+++ codes/09/3.31-mixed field-2.0.py
@@ -0,0 +1,31 @@
+import pylab as pl
+x_0=1.2
+y_0=0
+v_x_0=0
+v_y_0=0.5
+dt=0.01
+end_t=100
+x=[]
+y=[]
+v_x=[]
+v_y=[]
+t=[]
+x.append(x_0)
+y.append(y_0)
+v_x.append(v_x_0)
+v_y.append(v_y_0)
+t.append(0)
+q=1.6*10**-19
+m=1*10**-19
+b=1.0
+E=1.0
+for i in range(int(end_t/dt)):
+    v_x.append(v_x[-1]+q*b*v_y[-1]*dt/m+q*E*dt/m)
+    v_y.append(v_y[-1]-q*b*v_x[-1]*dt/m)
+    x.append(x[-1]+v_x[-1]*dt)
+    y.append(y[-1]+v_y[-1]*dt)
+    t.append(t[i]+dt)
+pl.plot(x,y)
+pl.xlabel(u'x',fontsize=14)
+pl.ylabel(u'y',fontsize=14)
+pl.show
